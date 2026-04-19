8fd6ef31feb6c823ec41a15df9772b257c452344
xinshoutw <me@xinshou.tw>
2026-04-19 21:28:27 +0800

    fix(web): add avatars.githubusercontent.com to next/image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,21 @@
 import type { NextConfig } from "next";
 import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+import createNextIntlPlugin from "next-intl/plugin";
 
 initOpenNextCloudflareForDev();
 
+const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
+
 const nextConfig: NextConfig = {
   transpilePackages: ["three"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
