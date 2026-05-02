e08c361da7c3c01d3e864436bcd1cc6702b55716
xinshoutw <me@xinshou.tw>
2026-05-02 10:20:22 +0800

    fix(constants): update app store URLs to point to TestFlight and GitHub releases

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
-export const APP_STORE_URL = 'https://apps.apple.com/app/tigerduck/id0000000000';
-export const PLAY_STORE_URL = 'https://play.google.com/store/apps/details?id=org.ntust.tigerduck';
+export const APP_STORE_URL = 'https://testflight.apple.com/join/eVt9Gjkw'; // TODO: Apple Store URL
+export const PLAY_STORE_URL = 'https://github.com/tigerduck-app/tigerduck-app-android/releases/latest';  // TODO: Google Play URL
 export const TESTFLIGHT_URL = 'https://testflight.apple.com/join/eVt9Gjkw';
 
 export const GITHUB_ORG_URL = 'https://github.com/tigerduck-app';
@@ -10,7 +10,6 @@ export const LICENSE_URL = 'https://github.com/tigerduck-app/tigerduck-app/blob/
 export const ANDROID_RELEASES_URL = 'https://github.com/tigerduck-app/tigerduck-app-android/releases';
 
 export const GDG_NTUST_URL = 'https://gdg-ntust.org';
-export const PRIVACY_URL = 'https://app.ntust.org/tigerduck/privacy';
 
 export const SUPPORT_FORM_URL = 'https://forms.gle/SDQTK59C1TSwgbUX8';
 export const DISCORD_URL = 'https://discord.gg/6kh8qFJmBZ';
